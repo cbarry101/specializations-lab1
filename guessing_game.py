615355c6add5bf06b514cd7df6209a0e55556b2e
Connor Barry <cbarry578@gmail.com>
2022-01-11 15:44:57 -0800

    changed guessing_game and added tries counter

--- guessing_game.py
+++ guessing_game.py
@@ -6,10 +6,11 @@ player_name = input('What is your name?\n')
 print(f"{player_name}, I'm thinking of a number between 1 and 100.")
 
 actual_number = random.randint(1,100)
-
+amount_of_tries = 0
 
 while(True):
     player_guess = int(input('Guess a number!'))
+    amount_of_tries += 1
     if (player_guess > actual_number):
         time.sleep(.5)
         print('Your guess is too high. Try again.')
@@ -18,5 +19,5 @@ while(True):
         print('Your guess is too low. Try again.')
     else:
         time.sleep(.5)
-        print(f"Nice guess {player_name}! {actual_number} was my number!")
+        print(f"Nice guess {player_name}! {actual_number} was my number! You got it in {amount_of_tries} attempts!")
         break
